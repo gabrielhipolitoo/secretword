6938921465ea925ef3780507eece11a885091017
Gabriel Hipolito <c.gabrielhipolitoh@gmail.com>
2023-04-03 20:31:26 -0300

    add componente

---
+++ src/Data/Palavras.js
@@ -0,0 +1,8 @@
+export const palavras =  {
+    carro:["motor","porta","capo","volante"],
+    fruta:["maça","banana","pera","uva"],
+    corpo:["mao","pe","olho","cabeca"],
+    computador:["mouse","teclado"],
+    programacao:["react","mongo","html","python"],
+    alimento:["arroz","feijao","carne","ovo"]
+}
